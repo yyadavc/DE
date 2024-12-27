4290715b42b473cb681a3aabe92443f8fdff793e
yashasyc@gmail.com <yashasyc@gmail.com>
2024-12-27 09:43:34 +0000

    push

--- Adventure-Works-Data-Engineering-Project/Databricks/scopes.py
+++ Adventure-Works-Data-Engineering-Project/Databricks/scopes.py
@@ -10,6 +10,11 @@
 # COMMAND ----------
 
 # MAGIC %md
+# MAGIC databricks secrets create-scope --scope AW-PR
+
+# COMMAND ----------
+
+# MAGIC %md
 # MAGIC databricks secrets put --scope AW-PR --key clientid --string-value "2d1b308e-8dcf-4898-8f42-10893d11fd05"
 
 # COMMAND ----------
