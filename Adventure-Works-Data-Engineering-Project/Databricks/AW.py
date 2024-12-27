e313685eb8ab061945360ae44ad5bfaa3d616336
yashasyc@gmail.com <yashasyc@gmail.com>
2024-12-27 07:21:55 +0000

    push

---
+++ Adventure-Works-Data-Engineering-Project/Databricks/AW.py
@@ -0,0 +1,8 @@
+# Databricks notebook source
+# MAGIC %sql
+# MAGIC SELECT * FROM aw_adb_pr.information_schema.catalog_privileges
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC
--- Adventure-Works-Data-Engineering-Project/Databricks/CODE.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-print("Hello World")
-
-# COMMAND ----------
-
-# MAGIC %md
-# MAGIC
