40e6976e0c5ed14cbda0631bec5a3e2bb414fca4
yashasyc@gmail.com <yashasyc@gmail.com>
2024-12-27 13:44:54 +0000

    push

--- Adventure-Works-Data-Engineering-Project/Databricks/silver_layer.py
+++ Adventure-Works-Data-Engineering-Project/Databricks/silver_layer.py
@@ -12,7 +12,7 @@
 spark.conf.set("fs.azure.account.auth.type.awidhdatalake.dfs.core.windows.net", "OAuth")
 spark.conf.set("fs.azure.account.oauth.provider.type.awidhdatalake.dfs.core.windows.net", "org.apache.hadoop.fs.azurebfs.oauth2.ClientCredsTokenProvider")
 spark.conf.set("fs.azure.account.oauth2.client.id.awidhdatalake.dfs.core.windows.net", "2d1b308e-8dcf-4898-8f42-10893d11fd05")
-spark.conf.set("fs.azure.account.oauth2.client.secret.awidhdatalake.dfs.core.windows.net", SVP_SECRET)
+spark.conf.set("fs.azure.account.oauth2.client.secret.awidhdatalake.dfs.core.windows.net", "MKG8Q~s2PixZR595_-xEslE4Xk~JGVeXn5F2mdy")
 spark.conf.set("fs.azure.account.oauth2.client.endpoint.awidhdatalake.dfs.core.windows.net", "https://login.microsoftonline.com/71a1a040-1e0a-436b-b27e-6118c249b254/oauth2/token")
 
 # COMMAND ----------
